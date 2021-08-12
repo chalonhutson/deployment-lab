0276de9dd3e2fc992a04e79a7db2aedb33d6d4c4
chalonhutson <chalonhutson@gmail.com>
2021-08-12 12:46:27 -0600

    Fixed some things.

--- server/server.js
+++ server/server.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.use(express.static("client"));
 
+
 app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "../client/index.html"))});
 
-
+app.get("/styles", (req, res) => {res.sendFile(path.join(__dirname, "../client/index.css"))});
 
 const port = process.env.PORT || 4004;
 
